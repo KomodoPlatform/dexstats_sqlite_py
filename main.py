8540af195631a87d7144137a51b7e952a7e910a0
Anton TonyL Lysakov <tlysakov@gmail.com>
2021-10-31 17:38:18 +0700

    add logo and long description

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ app.add_middleware(
 def get_exchange_info():
     exchange_info = {
         "name": "AtomicDEX",
-        "description": "The decentralized exchange on AtomicDEX uses P2P order books powered by atomic swaps that enable the transfer of cryptocurrency from one party to another, without the use of a third-party intermediary.",
+        "description": "AtomicDEX is a cryptocurrency wallet and decentralized exchange rolled into one application available on desktop, mobile, and web. It allows users to send, receive, and trade numerous coins such as Bitcoin, Ethereum, and BNB and ERC-20 and BEP-20 tokens. With AtomicDEX, there is no need to download the whole blockchain in the process. One of the AtomicDEX wallet’s main features is the use of a 24-word seed phrase that has to be kept safe or to be remembered by the user, as it is required to recover funds in the event of a forgotten password. The decentralized exchange on AtomicDEX uses P2P order books powered by atomic swaps that enable the transfer of cryptocurrency from one party to another, without the use of a third-party intermediary. AtomicDEX is one of the ecosystem products built with Komodo AtomicDEX API - an open-source framework that is compatible with 99% of cryptocurrencies. DogeDEX and GleecDEX are two additional examples of products that use Komodo AtomicDEX API. Try AtomicDEX Beta on any platform and start making atomic swaps today.",
         "location": "DEX",
-        "logo": "",
+        "logo": "https://komodo.live/static/AtomicDEX_Mark-Color.svg",
         "website": "https://atomicdex.io/",
         "twitter": "https://twitter.com/atomicdex",
         "version": "2.0",
