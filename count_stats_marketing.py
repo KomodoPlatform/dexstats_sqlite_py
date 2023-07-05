2a66e6f35c18e2fbe4e69118abd234f3ff3f0ca3
Anton "TonyL" Lysakov <tlysakov@gmail.com>
2023-07-05 18:03:10 +0200

    count actual data

--- count_stats_marketing.py
+++ count_stats_marketing.py
@@ -14,12 +14,16 @@ def check_if_pair_is_fresh(sql_coursor, pair, timestamp):
         return False
     return True
 
-path_to_db = "MM2.db"
+path_to_db = "/Users/antonlysakov/devel/MM2.db"
 
 timestamp_2020_start = 1577836800
 timestamp_2020_end   = 1609459200
 timestamp_2021_start = 1609459201
 timestamp_2021_end   = 1640995200
+timestamp_2022_start = 1640995201
+timestamp_2022_end   = 1672527599
+timestamp_2023_start = 1672527600
+timestamp_2023_end   = 1704063599
 
 usd_prices = requests.get("https://prices.komodo.earth:1313/api/v2/tickers").json()
 
@@ -38,6 +42,17 @@ swap_statuses_2021 = sql_coursor.fetchall()
 swaps_2021 = len(swap_statuses_2021)
 usd_volume_2021 = usd_volume_for_swap_statuses(swap_statuses_2021, usd_prices)
 
+
+sql_coursor.execute("SELECT * FROM stats_swaps WHERE started_at > ? AND started_at < ? AND is_success=1;", (timestamp_2022_start, timestamp_2022_end))
+swap_statuses_2022 = sql_coursor.fetchall()
+swaps_2022 = len(swap_statuses_2022)
+usd_volume_2022 = usd_volume_for_swap_statuses(swap_statuses_2022, usd_prices)
+
+
+sql_coursor.execute("SELECT * FROM stats_swaps WHERE started_at > ? AND started_at < ? AND is_success=1;", (timestamp_2023_start, timestamp_2023_end))
+swap_statuses_2023 = sql_coursor.fetchall()
+swaps_2023 = len(swap_statuses_2023)
+usd_volume_2023 = usd_volume_for_swap_statuses(swap_statuses_2023, usd_prices)
 
 available_pairs = get_availiable_pairs(path_to_db)
 # print("Total pairs: " + str(len(available_pairs)))
@@ -68,6 +83,40 @@ for pair in available_pairs:
 # sorting by volume
 pairs_volumes_2021 = dict(sorted(pairs_volumes_2021.items(), key=lambda item: item[1], reverse=True))
 
+pairs_volumes_2022 = {}
+
+for pair in available_pairs:
+    print("counting usd volume for " + str(pair))
+    t = (timestamp_2022_start,pair[0],pair[1],)
+    sql_coursor.execute("SELECT * FROM stats_swaps WHERE started_at > ? AND maker_coin_ticker=? AND taker_coin_ticker=? AND is_success=1;", t)
+    swap_for_pair_a = sql_coursor.fetchall()
+    pair_usd_volume_2022_a = usd_volume_for_swap_statuses(swap_for_pair_a, usd_prices)
+    sql_coursor.execute("SELECT * FROM stats_swaps WHERE started_at > ? AND taker_coin_ticker=? AND maker_coin_ticker=? AND is_success=1;", t)
+    swap_for_pair_b = sql_coursor.fetchall()
+    pair_usd_volume_2022_b = usd_volume_for_swap_statuses(swap_for_pair_b, usd_prices)
+    pair_usd_volume_2022 = pair_usd_volume_2022_a + pair_usd_volume_2022_b
+    pairs_volumes_2022[pair[0]+"_"+pair[1]] = int(pair_usd_volume_2022)
+
+# sorting by volume
+pairs_volumes_2022 = dict(sorted(pairs_volumes_2022.items(), key=lambda item: item[1], reverse=True))
+
+pairs_volumes_2023 = {}
+
+for pair in available_pairs:
+    print("counting usd volume for " + str(pair))
+    t = (timestamp_2023_start,pair[0],pair[1],)
+    sql_coursor.execute("SELECT * FROM stats_swaps WHERE started_at > ? AND maker_coin_ticker=? AND taker_coin_ticker=? AND is_success=1;", t)
+    swap_for_pair_a = sql_coursor.fetchall()
+    pair_usd_volume_2023_a = usd_volume_for_swap_statuses(swap_for_pair_a, usd_prices)
+    sql_coursor.execute("SELECT * FROM stats_swaps WHERE started_at > ? AND taker_coin_ticker=? AND maker_coin_ticker=? AND is_success=1;", t)
+    swap_for_pair_b = sql_coursor.fetchall()
+    pair_usd_volume_2023_b = usd_volume_for_swap_statuses(swap_for_pair_b, usd_prices)
+    pair_usd_volume_2023 = pair_usd_volume_2023_a + pair_usd_volume_2023_b
+    pairs_volumes_2023[pair[0]+"_"+pair[1]] = int(pair_usd_volume_2023)
+
+# sorting by volume
+pairs_volumes_2023 = dict(sorted(pairs_volumes_2021.items(), key=lambda item: item[1], reverse=True))
+
 conn.close()
 
 
@@ -75,9 +124,16 @@ print("Total swaps 2020: " + str(swaps_2020))
 print("USD volume 2020: " + str(usd_volume_2020))
 print("Total swaps 2021: " + str(swaps_2021))
 print("USD volume 2021: " + str(usd_volume_2021))
+print("Total swaps 2022: " + str(swaps_2022))
+print("USD volume 2022: " + str(usd_volume_2022))
+print("Total swaps 2023: " + str(swaps_2023))
+print("USD volume 2023: " + str(usd_volume_2023))
 # print("Total new pairs in 2021: " + str(fresh_pairs_count))
 # print("New pairs list: " + str(fresh_pairs))
-print("Writing info about volumes for pairs into pair_volumes.json")
-with open("pair_volumes.json", "w+") as file:
+print("Writing info about volumes for pairs into pair_volumes_year.json")
+with open("pair_volumes_2021.json", "w+") as file:
     file.write(json.dumps(pairs_volumes_2021))
-
+with open("pair_volumes_2022.json", "w+") as file:
+    file.write(json.dumps(pairs_volumes_2022))
+with open("pair_volumes_2023.json", "w+") as file:
+    file.write(json.dumps(pairs_volumes_2023))
