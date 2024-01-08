f5a363f7780d88a5fd5c859b5115e9be94db84e7
smk762 <smk762@iinet.net.au>
2024-01-09 02:34:58 +0800

    patch data from test-defi-stats

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_utils.tasks import repeat_every
 from logger import logger
+import requests
 import models
 import const
 
@@ -18,73 +19,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+DEFI_STATS_URL = "https://test.defi-stats.komodo.earth/api/v3/stats-api"
 
-@app.on_event("startup")
-@repeat_every(seconds=60)
-def cache_gecko_data():  # pragma: no cover
-    try:
-        cache.save.gecko_data()
-    except Exception as e:
-        logger.warning(f"{type(e)} Error in [cache_gecko_data]: {e}")
-
-
-@app.on_event("startup")
-@repeat_every(seconds=60)
-def cache_summary():  # pragma: no cover
-    try:
-        cache.save.summary()
-    except Exception as e:
-        logger.warning(f"{type(e)} Error in [cache_summary_data]: {e}")
-
-
-@app.on_event("startup")
-@repeat_every(seconds=60)
-def cache_ticker():  # pragma: no cover
-    try:
-        cache.save.ticker()
-    except Exception as e:
-        logger.warning(f"{type(e)} Error in [cache_ticker_data]: {e}")
-
-
-@app.on_event("startup")
-@repeat_every(seconds=600)  # caching data every 10 minutes
-def cache_atomicdexio():  # pragma: no cover
-    try:
-        cache.save.atomicdexio()
-    except Exception as e:
-        logger.warning(f"{type(e)} Error in [cache_atomicdex_io]: {e}")
-
-
-@app.on_event("startup")
-@repeat_every(seconds=600)  # caching data every 10 minutes
-def cache_atomicdex_fortnight():  # pragma: no cover
-    try:
-        cache.save.atomicdex_fortnight()
-    except Exception as e:
-        logger.warning(f"{type(e)} Error in [cache_atomicdex_io_fortnight]: {e}")
-
-
-@app.on_event("startup")
-@repeat_every(seconds=86400)
-def update_coins_config():  # pragma: no cover
-    try:
-        cache.save.coins_config()
-    except Exception as e:
-        logger.warning(f"{type(e)} Error in [update_coins_config]: {e}")
-
-
-@app.on_event("startup")
-@repeat_every(seconds=86400)
-def update_coins():  # pragma: no cover
-    try:
-        cache.save.coins()
-    except Exception as e:
-        logger.warning(f"{type(e)} Error in [update_coins]: {e}")
-
-
-# //////////////////////////// #
-# Routes retrieved from cache  #
-# //////////////////////////// #
+# /////////////////////////////////////////////////// #
+# Routes retrieved from test.defi-stats/komodo.earth  #
+# /////////////////////////////////////////////////// #
 @app.get('/api/v1/atomicdexio')
 def atomicdexio():
     '''
@@ -92,7 +31,7 @@ def atomicdexio():
     Updates every 10 minutes.
     '''
     try:
-        return cache.load.atomicdexio()
+        return requests.get(f"{DEFI_STATS_URL}/atomicdexio").json()
     except Exception as e:  # pragma: no cover
         logger.warning(f"{type(e)} Error in [/api/v1/atomicdexio]: {e}")
         return {"error": f"{type(e)} Error in [/api/v1/atomicdexio]: {e}"}
@@ -105,7 +44,7 @@ def atomicdex_fortnight():
     Updates every 10 minutes.
     '''
     try:
-        return cache.load.atomicdex_fortnight()
+        return requests.get(f"{DEFI_STATS_URL}/atomicdex_fortnight").json()
     except Exception as e:  # pragma: no cover
         logger.warning(f"{type(e)} Error in [/api/v1/atomicdex_fortnight]: {e}")
         return {"error": f"{type(e)} Error in [/api/v1/atomicdex_fortnight]: {e}"}
@@ -118,7 +57,7 @@ def summary():
     pairs traded in the last 7 days.
     '''
     try:
-        return cache.load.summary()
+        return requests.get(f"{DEFI_STATS_URL}/summary").json()
     except Exception as e:  # pragma: no cover
         logger.warning(f"{type(e)} Error in [/api/v1/summary]: {e}")
         return {"error": f"{type(e)} Error in [/api/v1/atomicdexio]: {e}"}
@@ -131,7 +70,7 @@ def ticker():
     for all pairs traded in the last 7 days.
     '''
     try:
-        return cache.load.ticker()
+        return requests.get(f"{DEFI_STATS_URL}/ticker").json()
     except Exception as e:  # pragma: no cover
         logger.warning(f"{type(e)} Error in [/api/v1/ticker]: {e}")
         return {"error": f"{type(e)} Error in [/api/v1/atomicdexio]: {e}"}
@@ -144,10 +83,7 @@ def ticker_v2():
     for all pairs traded in the last 7 days.
     '''
     try:
-        data = cache.load.ticker()
-        cleaned = {}
-        [cleaned.update(i) for i in data]
-        return cleaned
+        return requests.get(f"{DEFI_STATS_URL}/ticker").json()
     except Exception as e:  # pragma: no cover
         logger.warning(f"{type(e)} Error in [/api/v1/ticker]: {e}")
         return {"error": f"{type(e)} Error in [/api/v1/atomicdexio]: {e}"}
@@ -164,24 +100,7 @@ def orderbook(pair: str = "KMD_LTC") -> dict:
         pair: str (e.g. KMD_LTC)
     '''
     try:
-        if len(pair) > 32:
-            raise HTTPException(
-                status_code=400,
-                detail="Pair cant be longer than 32 symbols"
-            )  # pragma: no cover
-        elif "_" not in pair:
-            raise HTTPException(
-                status_code=400,
-                detail="Pair should be in format TICKER1_TICKER2"
-            )  # pragma: no cover
-        elif pair == "":
-            raise HTTPException(
-                status_code=400,
-                detail="Pair can not be empty. Use the format TICKER1_TICKER2"
-            )  # pragma: no cover
-        pair = models.Pair(pair)
-        orderbook = models.Orderbook(pair)
-        return orderbook.for_pair(True)
+        return requests.get(f"{DEFI_STATS_URL}/orderbook/{pair}").json()
     except Exception as e:  # pragma: no cover
         logger.warning(f"{type(e)} Error in [/api/v1/orderbook/{pair}]: {e}")
         return {"error": f"{type(e)} Error in [/api/v1/atomicdexio]: {e}"}
@@ -195,26 +114,7 @@ def trades(pair="KMD_LTC"):
         pair: str (e.g. KMD_LTC)
     '''
     try:
-        if len(pair) > 32:
-            raise HTTPException(
-                status_code=400,
-                detail="Pair cant be longer than 32 symbols"
-            )  # pragma: no cover
-        elif "_" not in pair:
-            raise HTTPException(
-                status_code=400,
-                detail="Pair should be in format TICKER1_TICKER2"
-            )  # pragma: no cover
-        elif pair == "":
-            raise HTTPException(
-                status_code=400,
-                detail="Pair can not be empty. Use the format TICKER1_TICKER2"
-            )  # pragma: no cover
-        DB = models.SqliteDB(const.MM2_DB_PATH, dict_format=True)
-        pair = models.Pair(pair)
-        trades_data = pair.trades(days=1)
-        DB.close()
-        return trades_data
+        return requests.get(f"{DEFI_STATS_URL}/trades/{pair}").json()
     except Exception as e:  # pragma: no cover
         logger.warning(f"{type(e)} Error in [/api/v1/trades/{pair}]: {e}")
         return {"error": f"{type(e)} Error in [/api/v1/atomicdexio]: {e}"}
@@ -224,11 +124,7 @@ def trades(pair="KMD_LTC"):
 def last_price_for_pair(pair="KMD_LTC"):
     '''Last trade price for a given pair.'''
     try:
-        pair = models.Pair(pair)
-        DB = models.SqliteDB(const.MM2_DB_PATH, dict_format=True)
-        last_price = DB.get_last_price_for_pair(pair.base, pair.quote)
-        DB.close()
-        return last_price
+        return requests.get(f"{DEFI_STATS_URL}/last_price/{pair}").json()
     except Exception as e:  # pragma: no cover
         logger.warning(f"{type(e)} Error in [/api/v1/last_price/{pair}]: {e}")
         return {"error": f"{type(e)} Error in [/api/v1/atomicdexio]: {e}"}
